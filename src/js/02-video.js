3241cd692ec23ff7fc8a6d4e46ff409b6a9475d7
Marry Tymofiichuk <masha432alex@i.ua>
2022-08-07 23:05:11 +0300

    add js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,19 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('timeupdate', function () {
-    console.log("update")
-});
+const LOCALSTORAGE_KEY = "videoplayer-current-time"
+const getCurrantTime = localStorage.getItem(LOCALSTORAGE_KEY)
+
+if (getCurrantTime !== null) {
+    player.setCurrentTime(getCurrantTime);
+}
+
+function onPlay({ seconds }) {
+    localStorage.setItem(LOCALSTORAGE_KEY, seconds)
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
