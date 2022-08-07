667a8dbebfdc709c4eb3c512deb298bc5bf3846f
Marry Tymofiichuk <masha432alex@i.ua>
2022-08-07 10:32:40 +0300

    add files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', function () {
+    console.log("update")
+});
